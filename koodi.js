fce2a0e6708a3606e5e628f2444f58f66574318e
Mikko Lehtinen <mikkoleh@office.nbl.fi>
2018-10-15 13:03:54 +0300

    luku toimii, enää testit, node projekti nyt

--- koodi.js
+++ koodi.js
@@ -1,21 +1,20 @@
+var fs = require('fs');
+
 //map a tetris pieces' relative coordinate to an actual one based on the coordinate where the first piece of a tetrus piece is
 const getAbsoluteCoordinates = (tetrisPiece, position) => {
 	return tetrisPiece.map(piece => {
 		return [piece[0] + position[0], piece[1] + position[1]];
 	});
 }
-//console.log(getAbsoluteCoordinates(tetrisPieces[0], [1,1])) // should return => [[1,1], [2,1], [2,2], [3,2]]
 
 //are two coordinates the same?
 const isTwoCoordinatesTheSame = (coor1, coor2) => {
 	return coor1[0] === coor2[0] && coor1[1] === coor2[1];
 }
-//console.log(isTwoCoordinatesTheSame([0,0], [0,0])); //=> true
-//console.log(isTwoCoordinatesTheSame([0,0], [0,1])); // => false
 
 // coordinate is in the array?
 const coordinateExistsInArray = (coordinate, availableCoordinates) => {
-  for (let i = 0; i < availableCoordinates.length; i++) {
+	for (let i = 0; i < availableCoordinates.length; i++) {
 		if (isTwoCoordinatesTheSame(coordinate, availableCoordinates[i])) {
 			return true;
 		}
@@ -23,33 +22,28 @@ const coordinateExistsInArray = (coordinate, availableCoordinates) => {
 	return false;
 };
 
-//console.log(coordinateExistsInArray([0,0], [[0,0], [0,1], [1,1]])); //=> should return true
-//console.log(coordinateExistsInArray([5,0], [[0,0], [0,1], [1,1]])); //=> should return false
-
 // if coordinate found, remove, return new array from where the removals have been done
 const removeCoordinates = (coordinatestoRemove, availableCoordinates) => {
 	return availableCoordinates.filter(el => {
 		return !coordinateExistsInArray(el, coordinatestoRemove);
 	});
 };
-//console.log(removeCoordinates([[0,0], [1,1]], [[0,0], [0,1], [1,1]])); //=> should return [0,1]
 
 // print a tetris piece
 const printPiece = (piece) => {
-	return `${piece[0][0]},${piece[0][1]}; ${piece[1][0]},${piece[1][1]}; ${piece[2][0]},${piece[2][1]}; ${piece[3][0]},${piece[3][1]}`
+	return `${piece[0][0]},${piece[0][1]};${piece[1][0]},${piece[1][1]};${piece[2][0]},${piece[2][1]};${piece[3][0]},${piece[3][1]}`
 }
 
 //print the result
-const printResult = (pieceA, pieceB, pieceC, pieceD) => {
-	console.log(`A: ${printPiece(pieceA)}`);
-	console.log(`B: ${printPiece(pieceB)}`);
-	console.log(`C: ${printPiece(pieceC)}`);
-	console.log(`D: ${printPiece(pieceD)}`);
+const printResult = (ids, pieceA, pieceB, pieceC, pieceD) => {
+	console.log(`${ids[0]}:${printPiece(pieceA)}`);
+	console.log(`${ids[1]}:${printPiece(pieceB)}`);
+	console.log(`${ids[2]}:${printPiece(pieceC)}`);
+	console.log(`${ids[3]}:${printPiece(pieceD)}`);
 }
-//console.log(printResult(tetrisPieces[0], tetrisPieces[1], tetrisPieces[2], tetrisPieces[3]));
 
 //the function that does all the work, handle state etc.
-const fitToGrid = (tetrisPieces) => {
+const fitToGrid = (ids, tetrisPieces) => {
 	const grid = [
 		[0, 0],
 		[0, 1],
@@ -87,7 +81,7 @@ const fitToGrid = (tetrisPieces) => {
 				const gridAfterABCD = removeCoordinates(coordinatesD, gridAfterABC);
 
 				if (gridAfterABCD.length === 0) {
-					printResult(coordinatesA, coordinatesB, coordinatesC, coordinatesD);
+					printResult(ids, coordinatesA, coordinatesB, coordinatesC, coordinatesD);
 					return;
 				}
 			}
@@ -95,31 +89,28 @@ const fitToGrid = (tetrisPieces) => {
 	}
 }
 
-//1. starting point,
-const tetrisPieces = [
-	[
-		[0, 0],
-		[1, 0],
-		[1, 1],
-		[2, 1]
-	],
-	[
-		[0, 0],
-		[0, 1],
-		[0, 2],
-		[1, 2]
-	],
-	[
-		[0, 0],
-		[1, 0],
-		[2, 0],
-		[1, 1]
-	],
-	[
-		[0, 0],
-		[1, 0],
-		[1, 1],
-		[1, -1]
-	]
-];
-	fitToGrid(tetrisPieces);
+//create array for id's
+const parseId = (line) => {
+	return line.substr(0, line.indexOf(':'));
+}
+
+//create array for coordinates
+const parseTetrisPiece = (line) => {
+	let coordinates = []
+	let lineWithoutID = line.substr(line.indexOf(':') + 1, line.length);
+	let coordinatesInStringArray = lineWithoutID.replace(/\r/g, "").split(';');
+	for (let i = 0; i < coordinatesInStringArray.length; i++) {
+		coordinates.push((JSON.parse("[" + coordinatesInStringArray[i] + "]")));
+	}
+	return coordinates;
+}
+
+//1. starting point, read palikkatiedosto.txt and create one array for id's and one for coordinates
+let lines = fs.readFileSync('palikkatiedosto.txt').toString().split("\n");
+let ids = [];
+let tetrisPieces = [];
+for (i in lines) {
+	ids.push(parseId(lines[i]));
+	tetrisPieces.push(parseTetrisPiece(lines[i]));
+}
+fitToGrid(ids, tetrisPieces);
--- playground.js
+++ playground.js
@@ -33,3 +33,51 @@ const tetrisPieces = [
 		[2, -1]
 	]
 ];
+
+/*
+fs.readFile('palikkatiedosto.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    const ids = getListOfIds(data);
+    const tetrisPieces = getTetrisPieces(data);
+
+});*/
+
+
+//for testing purposes
+const tetrisPiecesTest = [
+	[
+		[0, 0],
+		[1, 0],
+		[1, 1],
+		[2, 1]
+	],
+	[
+		[0, 0],
+		[0, 1],
+		[0, 2],
+		[1, 2]
+	],
+	[
+		[0, 0],
+		[1, 0],
+		[2, 0],
+		[1, 1]
+	],
+	[
+		[0, 0],
+		[1, 0],
+		[1, 1],
+		[1, -1]
+	]
+];
+
+//fitToGrid(tetrisPieces);
+
+//console logeja:
+//console.log(getAbsoluteCoordinates(tetrisPieces[0], [1,1])) // should return => [[1,1], [2,1], [2,2], [3,2]]
+//console.log(isTwoCoordinatesTheSame([0,0], [0,0])); //=> true
+//console.log(isTwoCoordinatesTheSame([0,0], [0,1])); // => false
+//console.log(coordinateExistsInArray([0,0], [[0,0], [0,1], [1,1]])); //=> should return true
+//console.log(coordinateExistsInArray([5,0], [[0,0], [0,1], [1,1]])); //=> should return false
+//console.log(removeCoordinates([[0,0], [1,1]], [[0,0], [0,1], [1,1]])); //=> should return [0,1]
+//console.log(printResult(tetrisPieces[0], tetrisPieces[1], tetrisPieces[2], tetrisPieces[3]));
